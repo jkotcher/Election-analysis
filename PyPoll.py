b2a12ee8bbeec617330a554a9bdcde9bbe93cff7
jkotcher <jacob.kotcher@gmail.com>
2022-06-15 11:41:54 -0400

    Creating CSV file

---
+++ PyPoll.py
@@ -0,0 +1,20 @@
+# The data we need to retrieve
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on the popular vote.
+import csv
+# Assign a variable for the file to load and the path
+file_to_load = 'Resources/election_results.csv'
+
+#Open the election results
+with open(file_to_load) as election_data:
+
+
+# To do: Perform analysis
+    print(election_data)
+
+
+# Close the file
+
