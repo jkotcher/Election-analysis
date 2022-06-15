e9c821b87df5fb1a7411859eaa72011f456c8e3c
jkotcher <jacob.kotcher@gmail.com>
2022-06-15 12:03:51 -0400

    creating output file

--- PyPoll.py
+++ PyPoll.py
@@ -5,16 +5,19 @@
 # 4. The total number of votes each candidate won
 # 5. The winner of the election based on the popular vote.
 import csv
+import os
 # Assign a variable for the file to load and the path
-file_to_load = 'Resources/election_results.csv'
+file_to_load = os.path.join("Resources", "election_results.csv")
 
 #Open the election results
 with open(file_to_load) as election_data:
 
 
-# To do: Perform analysis
+# print the file object
     print(election_data)
 
 
-# Close the file
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+open(file_to_save, "w")
 
